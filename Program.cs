285b5cd75e8cdd66f0b0222f8b8fbb7c3945a8c3
Nikolay Botev <bono8106@yahoo.com>
2011-12-31 02:24:25 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpAsyncFutures
+{
+    class Program
+    {
+        static void log(Object message)
+        {
+            Console.WriteLine("[" + Thread.CurrentThread.ManagedThreadId + "] " + message);
+        }
+
+        static void Main(string[] args)
+        {
+            log("Main enter");
+            Client();
+            log("Main leave");
+            Console.ReadLine();
+        }
+
+        static async void Client()
+        {
+            log("Client enter");
+            var future = Service(10);
+            var x = await future;
+            log("Client got future result " + x);
+            Thread.Sleep(1000);
+            log("Client future callback done.");
+            log("Client leave");
+        }
+
+        static async Task<int> Service(int x)
+        {
+            log("Service enter");
+            try
+            {
+                return await Task.Run(() => x * x);
+            }
+            finally
+            {
+                log("Service leave");
+            }
+        }
+    }
+}
